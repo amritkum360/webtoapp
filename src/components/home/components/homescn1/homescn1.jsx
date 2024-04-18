94632d8bc73da0a073b5ec89a27c30195a1e8350
Amrit Kumar <amritkum360@gmail.com>
2024-04-18 16:49:35 +0530

    designed the one part of the home page

--- src/components/home/components/homescn1/homescn1.jsx
+++ src/components/home/components/homescn1/homescn1.jsx
@@ -4,21 +4,23 @@ import { Container, Row, Col, Button } from 'react-bootstrap';
 
 export default function Homescn1() {
     return (
-        <Container fluid className="container container_homescn1">
+        <Container fluid className="container_fluid container_homescn1">
             <Row className="row row_homescn1">
-                <Col md={6} className="text text_homescn1">
+                <Col md={12} className="text text_homescn1">
                     <div className="content content_homescn1">
-                        <h1 className="heading heading_homescn1">Convert Your Website Into an App</h1>
-                        <p className="sub-heading sub-heading_homescn1">It Takes Only 1 Minute</p>
-                        <div className="input-group input-group_homescn1">
+                        <h1 className="heading heading_homescn1 " >Convert Your Website Into an App</h1>
+                        <p className="sub-heading sub-heading_homescn1">Takes 1 Minute to Convert</p>
+                        {/* <div className="input-group input-group_homescn1"> */}
+                        <div>
                             <input className="input-field input-field_homescn1" placeholder="Enter your website URL" />
                             <Button className="convert-button convert-button_homescn1">Convert</Button>
+                            </div>
                         </div>
-                    </div>
+                    {/* </div> */}
                 </Col>
-                <Col md={6} className="image image_homescn1">
+                {/* <Col md={6} className="image image_homescn1">
                     <img className="image image_homescn1" src="https://appmaker.xyz/_next/image?url=%2Fimages%2Fpages%2Fshopify%2Fshopify-hero-image.png&w=640&q=75" alt="Your Image" />
-                </Col>
+                </Col> */}
             </Row>
         </Container>
     );
