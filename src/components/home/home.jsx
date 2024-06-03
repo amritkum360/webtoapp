bacf65fc4ca3085fd9a937c57c898406b261d192
Amrit Kumar <amritkum360@gmail.com>
2024-06-03 12:19:12 +0530

    added footerbar

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,4 +1,5 @@
 import Navbar from "../Navbar/Navbar";
+import FooterBar from "../Navbar/footerbar";
 import Pricing from "../Pricing/Pricing";
 import Homescn1 from "./components/homescn1/homescn1";
 import Homescn2 from "./components/homescn2/homescn2";
@@ -10,6 +11,7 @@ export default function Home(){
         <Homescn1 />
         <Homescn2 />
         <Pricing />
+        <FooterBar />
         {/* <img src="/1.png" alt="" style={{width:"100vw"}} />
         <img src="/2.png" alt="" style={{width:"100vw"}} /> */}
         </>
